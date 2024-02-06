d1c5bdf9c2872943f88c8966fa84d2d27dd6e832
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2024-02-07 02:14:59 +0530

    loop detection

---
+++ src/PhaseOne/LoopDetection.java
@@ -0,0 +1,62 @@
+package PhaseOne;
+
+import java.util.*;
+
+
+public class LoopDetection {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.push(20);
+        list.push(41);
+        list.push(5);
+        list.push(10);
+        //5.Creating a loop for testing
+        list.head.next.next.next.next = list.head;
+        list.detectLoopAndPrint();
+    }
+
+}
+
+
+class LinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    Node head;
+
+    void push(int newData) {
+        Node newNode = new Node(newData);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    void detectLoopAndPrint() {
+        Node slow = head, fast = head;
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                System.out.println("Loop found");
+                return;
+            }
+        }
+        System.out.println("No loop found");
+    }
+
+}
+
+
+/*
+ *
+ * Time complexity - O(N)
+ *
+ * Space complexity - O(1)
+ *
+ * */
