49dcdb4545be45cb8737c54cac6d64380b64eb61
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2024-02-07 01:33:51 +0530

    celebrity problem

--- src/PhaseOne/CelebrityProblem.java
+++ src/PhaseOne/CelebrityProblem.java
@@ -1,7 +1,43 @@
 package PhaseOne;
 
+import java.io.*;
+import java.util.*;
+
 public class CelebrityProblem {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] relationshipMatrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                relationshipMatrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int celebID = new Solution().findCeleb(relationshipMatrix, n);
+        if (celebID != -1) System.out.println("Celebrity ID is: " + celebID);
+        else System.out.println("No celebrity exist");
+
+        sc.close();
+    }
+}
+
+class Solution {
+    int findCeleb(int[][] m, int n) {
+
+        int candidate = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (m[candidate][i] == 1) candidate = i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (i != candidate && (m[candidate][i] == 1 || m[i][candidate] == 0)) return -1;
+        }
+
+
+        return candidate;
 
     }
 }
